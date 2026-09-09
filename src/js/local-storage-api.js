83a8c4086177b551474dc68cf179d0d85e5cdfb5
Oleksandra Bondarenko <bondarennko2106@gmail.com>
2026-09-09 19:40:05 +0300

    add delete task function

--- src/js/local-storage-api.js
+++ src/js/local-storage-api.js
@@ -1,11 +1,14 @@
+// об'єкт із ключами для localStorage
 export const LS_KEYS = {
   tasks: 'tasks',
 };
 
+// функція для збереження даних у localStorage
 export function saveState(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
+// функція для отримання даних із localStorage
 export function getState(key) {
   const data = localStorage.getItem(key);
   return data ? JSON.parse(data) : null;
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,4 @@
+// об'єкт refs зберігає посилання на потрібні елементи сторінки
 export const refs = {
   form: document.querySelector('.header-form'),
   taskList: document.querySelector('.tasks-list'),
